0e9bb15e15430526daf4f6b7ee884353b54ee4d8
Muthukumar M <muthukumar@kissflow.com>
2023-09-02 19:53:11 +0530

    basic notification done

---
+++ index.js
@@ -0,0 +1,38 @@
+import notifier from "node-notifier";
+import fetch from "node-fetch";
+import { load } from "cheerio";
+
+const INTERVAL = 1000 * 60 * 10; //TEN MIN
+
+async function checkStatus() {
+  console.log("ran at", new Date().toTimeString());
+  console.log("Getting Html");
+  try {
+    const html = await fetch("http://192.168.225.1/", {
+      insecureHTTPParser: true,
+    });
+    const body = await html.text();
+
+    if (html) {
+      console.log("Got html");
+      const $ = load(body);
+      const batteryLevel = parseInt($("#batterylevel").val());
+      console.log("value of charging", batteryLevel);
+      if (batteryLevel < 70) {
+        notifier.notify({
+          title: `Jio Fi battery status - ${$("#batterylevel").val()}`,
+          message: "🛑🛑 Please charge your jiofi🛑🛑 ",
+        });
+      }
+    } else throw new Error("No html doc found");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function run() {
+  checkStatus();
+  setInterval(checkStatus, INTERVAL);
+}
+
+run();
